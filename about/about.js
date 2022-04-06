9f9f34e7744c6ce0cff65eab7b6aee23fe8c37d3
amine <zouariamine52@gmail.com>
2022-04-06 18:48:14 +0100

    build

--- about/about.js
+++ about/about.js
@@ -4,11 +4,6 @@ const search = document.getElementById('search')
 const input = document.querySelector('.input')
 const form = document.querySelector('form')
 
-const pageOne = document.querySelector('.page-one')
-const pageTwo = document.querySelector('.page-two')
-const btnOne = document.getElementById('btnOne')
-const btnTwo = document.getElementById('btnTwo')
-
 search.addEventListener('click', (event) => {
   hideNav()
   search.classList.add('active')
@@ -37,18 +32,4 @@ function showNav() {
 form.addEventListener('submit', (event) => {
   event.preventDefault()
   window.find(input.value)
-})
-
-btnOne.addEventListener('click', () => {
-  pageOne.classList.remove('hidden')
-  pageTwo.classList.add('hidden')
-  btnOne.classList.add('active')
-  btnTwo.classList.remove('active')
-})
-
-btnTwo.addEventListener('click', () => {
-  pageTwo.classList.remove('hidden')
-  pageOne.classList.add('hidden')
-  btnTwo.classList.add('active')
-  btnOne.classList.remove('active')
 })
